210311b515b3c306d4eb24565fda5441b4a7b028
Kunark Khewal <kunark.khewal88@gmail.com>
2022-06-02 15:18:44 +0530

    added migration to create users, tweets and following tables

--- db/migrations/20220601133937_init.js
+++ db/migrations/20220601133937_init.js
@@ -3,11 +3,27 @@
  * @returns { Promise<void> }
  */
 exports.up = async function(knex) {
-    await knex.schema.createTable('persons', table => {
-        table.increments('id').primary();
-        table.integer('parentId').references('persons.id');
-        table.string('firstName');
-    });
+    await knex.schema
+        .createTable('users', table => {
+            table.increments('id').primary();
+            table.string('name').notNullable();
+            table.string('username').notNullable().unique();
+            table.string('password').notNullable();
+            table.string('location').notNullable();
+            table.timestamps(true, true);
+        })
+        .createTable('tweets', table => {
+            table.increments('id').primary();
+            table.integer('userid').references('id').inTable('users');
+            table.string('content').notNullable();
+            table.timestamps(true, true);
+        })
+        .createTable('followings', table => {
+            table.increments('id').primary();
+            table.integer('follower').references('id').inTable('users');
+            table.integer('following').references('id').inTable('users');
+            table.timestamps(true, true);
+        });
 };
 
 /**
@@ -15,5 +31,8 @@ exports.up = async function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('persons')
+    return knex.schema
+        .dropTableIfExists('users')
+        .dropTableIfExists('tweets')
+        .dropTableIfExists('followings')
 };
