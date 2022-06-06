7ddd9f7656499f2544c7b52fc405ed0b8abb5341
Kunark Khewal <kunarkkhewal@Kunarks-MacBook-Pro.local>
2022-06-06 16:50:11 +0530

    added cors and body-parser and changed port to 5000

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const routes = require('./routes');
 const dbSetup = require('./db/db-setup');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const app = express();
 dbSetup();
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
 app.use(express.json());
 app.use('/', routes)
 
